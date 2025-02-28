c24a60b1b0c118d168bd5d4c759a9d566a457840
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-28 15:52:19 +0100

    [feat] import "GlobalContext" and drestucturing of the object [fix] props

--- src/components/ProductUser.jsx
+++ src/components/ProductUser.jsx
@@ -1,12 +1,20 @@
+// Importo l'utilizzo di Context e il GlobalContenxt
+import { useContext } from "react";
+import GlobalContext from './../contexts/GlobalContext'
+
 import ProductCard from "./ProductCard";
 
-const ProductUser = (props) => {
+const ProductUser = () => {
+
+    // Destructuring
+    const { articles } = useContext(GlobalContext);
 
     // Articoli
     return (
         <>
             {/* Importo i dati da Home.jsx */}
-            {props.articlesApp.map((article) => (
+            {/* {props.articlesApp.map((article) => ( */}
+            {articles.map((article) => (
                 <ProductCard key={article.id} article={article} />
             ))
             }

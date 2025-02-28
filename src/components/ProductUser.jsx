21ad617edb884283416f4f373be974d7a311079f
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-28 15:05:08 +0100

    [fix] export api call to App

--- src/components/ProductUser.jsx
+++ src/components/ProductUser.jsx
@@ -1,29 +1,12 @@
 import ProductCard from "./ProductCard";
 
-import { useState, useEffect } from "react";
-
-// Importo axios
-import axios from "axios"
-
-// export default function input() {
-const ProductUser = () => {
-
-    // Stato dell'articolo (gestisce l'arrey di articoli)
-    const [articles, setListArticles] = useState([]);
-
-    // funzione di gestione chiamata all'API
-    function fetchArticles() {
-        axios.get("http://localhost:3000/posts")
-            .then((res) =>
-                setListArticles(res.data))
-    }
-    // richiamo la funzione di richiesta dati al caricamento del componente
-    useEffect(fetchArticles, []);
+const ProductUser = (props) => {
 
     // Articoli
     return (
         <>
-            {articles.map((article) => (
+            {/* Importo i dati da Home.jsx */}
+            {props.articlesApp.map((article) => (
                 <ProductCard key={article.id} article={article} />
             ))
             }
